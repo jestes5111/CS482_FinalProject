9016dc98de830165b532f0e78f3b43b62ba47134
Jesse Estes <jestes5111@gmail.com>
2021-09-11 12:24:35 -0400

    Made some readability changes to the KNN test. Tried working with LogisticRegressionCV but could not get it to work

--- LogisticRegression_Keyboard.py
+++ LogisticRegression_Keyboard.py
@@ -10,15 +10,19 @@ file = pd.read_csv("DSL-StrongPasswordData-modified.csv")
 # save the data as a np array
 data = file.to_numpy()
 
+# slice the data for easier reading
+X = data[:, :-2]
+y = data[:, -1]
+
 # use an 80/20 split for training and testing data
-X_train, X_test, y_train, y_test = train_test_split(data[:, 0:-2], data[:, -1])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
 # create and train the model with no cross validation
-lr = LogisticRegression(max_iter = 1000).fit(X_train, y_train)
+regressor = LogisticRegression(max_iter = 1000).fit(X_train, y_train)
 
 # save the train and test scores
-train_score = lr.score(X_train, y_train)
-test_score = lr.score(X_test, y_test)
+train_score = regressor.score(X_train, y_train)
+test_score = regressor.score(X_test, y_test)
 
 # print the scores
 print("Training score:", train_score)
